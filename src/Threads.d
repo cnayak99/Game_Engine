faa0e86305e9a7bad15f0be4f7cb83b6e6abb3ad
lssharpe <lssharpe@ncsu.edu>
2024-10-02 22:21:52 -0400

    Tested changes. Still works.

--- src/Threads.d
+++ src/Threads.d
@@ -1,7 +1,15 @@
 src/Threads.o: src/Threads.cpp include/Timeline.h src/Entity.h \
- include/Rectangle.h include/structs.h include/Entity.h
+ include/Rectangle.h include/Intersect.h include/Rectangle.h \
+ include/structs.h include/Entity.h include/Threads.h include/Timeline.h \
+ include/Intersect.h include/structs.h
 include/Timeline.h:
 src/Entity.h:
 include/Rectangle.h:
+include/Intersect.h:
+include/Rectangle.h:
 include/structs.h:
 include/Entity.h:
+include/Threads.h:
+include/Timeline.h:
+include/Intersect.h:
+include/structs.h:
